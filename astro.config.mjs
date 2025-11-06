0f8a4ad9c50ae1b48fedf2d83a9d34095a937812
Collins Mwangi <mwangicollins391@gmail.com>
2025-11-06 06:53:30 +0300

    Now node_modules shouldn't be here - how careless of me

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://collinsmwangi.up.railway.app',
+
   integrations: [
     tailwind(), 
     svelte()
